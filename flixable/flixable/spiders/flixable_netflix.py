87d39e21a88fae9f4e19192c7eed2929bc4e2132
Vignesh P <vignesh.p1110@gmail.com>
2020-07-11 19:35:45 +0530

    created parser for NetflixList

--- flixable/flixable/spiders/flixable_netflix.py
+++ flixable/flixable/spiders/flixable_netflix.py
@@ -7,4 +7,16 @@ class NetflixList(scrapy.Spider):
         'https://flixable.com/?min-rating=0&min-year=1920&max-year=2020&order=year#filterForm']
 
     def parse(self, response):
-        pass
+        titles = response.css("div.item")
+        for title in titles:
+            title_url = title.css(
+                "div.card-body a::attr(href)").extract_first()
+            title_name = title.css("h5.card-title::text").extract_first()
+            title_id = title.css(
+                "span.imdbRatingPlugin::attr(data-title)").extract_first()
+
+            yield {
+                'url': title_url,
+                'name': title_name,
+                'id': title_id
+            }
