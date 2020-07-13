567cf2186abb07bbb471e0f3ae2d06bbe7b51543
Vignesh P <vignesh.p1110@gmail.com>
2020-07-14 02:36:17 +0530

    added details -- not working

--- flixable/flixable/spiders/flixable_netflix.py
+++ flixable/flixable/spiders/flixable_netflix.py
@@ -23,6 +23,7 @@ class NetflixList(scrapy.Spider):
                     "span.imdbRatingPlugin::attr(data-title)").extract_first()
                 details = scrapy.Request(
                     url=self.base_url+item['title_url'], callback=self.parse_title)
+                item.update(details)
                 yield item
             self.page_num = self.page_num + 1
             # yield scrapy.Request(url=self.api_url.format(next_page), callback=self.parse)
@@ -30,9 +31,22 @@ class NetflixList(scrapy.Spider):
 
     def parse_title(self, response):
         data = response.css("div.section-sections")
-        release_year, title_certification, length = data.css(
+        item = FlixableItem()
+        item['release_year'], item['title_certification'], item['length'] = data.css(
             "h6.card-category").css("span::text").extract()
-        description = data.css("p.card-description::text").extract_first()
+        item['description'] = data.css(
+            "p.card-description::text").extract_first()
         for para in data.css("p"):
             if para.css("span.mr-2::text").extract_first() == 'Genres:':
-                genres = para.css("a::attr(href)").extract()
+                item['genre'] = para.css("a::text").extract()
+            elif para.css("span.mr-2::text").extract_first() == 'Cast:':
+                item['cast'] = para.css("a::text").extract()
+            elif para.css("span.mr-2::text").extract_first() == 'Production Country:':
+                item['production_country'] = para.css("a::text").extract()
+            elif para.css("span.mr-2::text").extract_first() == '\n                    Added to Netflix:':
+                item['added_date'] = para.css("span::text")[1].extract()
+
+            """ elif para.css("span.imdbRatingPlugin"):
+                item['imdb_rating'] = para.css("span.imdbRatingPlugin").css("span.rating::text") """
+
+        return item
