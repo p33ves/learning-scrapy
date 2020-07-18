f03ccefdb851658f8857f360b89b5e10b101d888
Vignesh P <vignesh.p1110@gmail.com>
2020-07-18 23:57:33 +0530

    pipeline updates list --- wip

--- flixable/flixable/pipelines.py
+++ flixable/flixable/pipelines.py
@@ -3,12 +3,13 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 # useful for handling different item types with a single interface
-
-import pymongo
+import json
+from datetime import datetime
 
 from itemadapter import ItemAdapter
-
 from flixable.items import FlixableListItem, FlixableNetflixItem
+
+import pymongo
 
 
 class FlixablePipeline:
@@ -21,4 +22,14 @@ class FlixablePipeline:
     def process_item(self, item, spider):
         if isinstance(item, FlixableNetflixItem):
             self.collection.insert(dict(item))
+        elif isinstance(item, FlixableListItem):
+            list_file = open("data/netflix_list.json")
+            list_data = json.load(list_file)
+            list_file.close()
+            if 'extract_date' not in list_data.keys() or list_data['extract_date'] != datetime.today().strftime('%Y-%m-%d'):
+                with open("data/netflix_list.json", 'w') as list_json:
+                    json.dump(
+                        {'extract_date': datetime.today().strftime('%Y-%m-%d')}, list_json)
+            with open("data/netflix_list.json", 'a') as list_json:
+                json.dump({item['title_url']: item}, list_json)
         return item
